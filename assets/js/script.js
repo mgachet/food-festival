774aec229375bf8d6b775858f32538ecdc736214
Marcelo Gachet <marcelo@growdnd.com>
2022-06-02 17:01:04 -0700

    service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-require("bootstrap");
+require('bootstrap');
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
-$(document).ready(function () {
-
-
-
+$(document).ready(function() {
   // First image is hard coded in index.html
-  const img1 = require("../../assets/img/food-table.jpg");
-  const img2 = require("../../assets/img/grill.jpg");
-
   const carouselSlides = [
     {
       title: "We travel all over the US",
@@ -23,28 +19,26 @@ $(document).ready(function () {
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
-    <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
-      <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
-          <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <h2 class="display-4 mb-2">${slide.title}</h2>
-            </div>
-            <div class="row align-items-center justify-content-center"> 
-              <h3>${slide.subtitle}</h3>
-            </div>
-            <div class=" mt-4 row align-items-center justify-content-center"> 
-              <a class="btn btn-primary" href="${slide.btnUrl}">
-                  ${slide.btnText}
-              </a>
-            </div>
+  <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
+    <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+              <h2 class="display-4 mb-2">${slide.title}</h2>
           </div>
-      </div>
-    </div>`)
+          <div class="row align-items-center justify-content-center"> 
+            <h3>${slide.subtitle}</h3>
+          </div>
+          <div class=" mt-4 row align-items-center justify-content-center"> 
+            <a class="btn btn-primary" href="${slide.btnUrl}">
+                ${slide.btnText}
+            </a>
+          </div>
+        </div>
+    </div>
+  </div>`)
   })
-
 });
